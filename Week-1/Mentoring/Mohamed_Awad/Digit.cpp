94d963cfdf91e3b29ab5ac48addd500243efdabb
awad1236 <awad123612@gmail.com>
2024-11-08 13:24:27 +0200

    mentoring sessions

---
+++ Week-1/Mentoring/Mohamed_Awad/Digit.cpp
@@ -0,0 +1,15 @@
+
+// https://codeforces.com/group/iEA3POa8Ps/contest/270648/problem/C
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int n; cin >> n; 
+char m; cin >> m; 
+cout << m << endl;
+
+}
