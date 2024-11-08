8b83ec2f3cb6a5eab1e6e3cdfafdda8f08777d21
tasbeehismail <tsbeehismaill@gmail.com>
2024-11-08 13:33:43 +0200

    Add pratice solutions

---
+++ Week-1/Practice/Canals-of-Beersheba.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    
+    int n; cin >> n;
+    cout << n / 4;
+    
+    return 0;
+}
+/*
+ 
+ */
---
+++ Week-1/Practice/Digit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n; cin >> n;
+
+    char res; cin >> res;
+    
+    cout << res << endl;
+
+    return 0;
+}
