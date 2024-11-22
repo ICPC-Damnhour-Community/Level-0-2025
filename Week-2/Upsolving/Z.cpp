df493f0672d68c1daafdd17b3bd2d64c5d5e7f62
Reem Elsayed Ghareeb <92355032+reemghareeb25@users.noreply.github.com>
2024-11-23 01:49:01 +0200

    Create Z.cpp

---
+++ Week-2/Upsolving/Z.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define bye return 0
+
+int main() {
+    long double n;
+    cin >> n;
+    if(n <= 127)
+        cout << "byte";
+    else if(n <= 32767)
+        cout << "short";
+    else if(n <= 2147483647)
+        cout << "int";
+    else if(n <= 9223372036854775807)
+        cout << "long";
+    else
+        cout << "BigInteger";
+    bye;
+}
