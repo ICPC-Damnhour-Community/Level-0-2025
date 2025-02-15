1ceca8ad8e3ab82b3746de3493ea407247aad664
Mohamed_Awad <152029138+awad1236@users.noreply.github.com>
2025-02-16 00:22:36 +0200

    Update Rotated_String.cpp

--- week-8/Contest-Upsolving/Rotated_String.cpp
+++ week-8/Contest-Upsolving/Rotated_String.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
-void shift(int n,int x[]){
-    int tmp = x[0];
-    for(int i = 0; i < n; i++) x[i] = x[i + 1];
-    x[n] = tmp;
-}
+
 int main() {
     int n,x;
     cin >> n >> x;
